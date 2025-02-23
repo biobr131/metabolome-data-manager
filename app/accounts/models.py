913527aa09dbd85bda2b9cfc68ee23b8df3c4899
biobr131 <takayuki.yamada.19@gmail.com>
2025-02-24 08:46:07 +0900

    updated the CustomUser model

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,9 +1,11 @@
 import uuid
 
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
 )
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
@@ -28,35 +30,51 @@ class CustomUserManager(BaseUserManager):
             email=self.normalize_email(email),
             password=password
         )
-        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
     id = models.UUIDField(
         default=uuid.uuid4,
         primary_key=True,
         editable=False
     )
     username = models.CharField(
-        verbose_name=_("username"),
+        _("username"),
         max_length=20,
-        unique=True
+        unique=True,
+        help_text=_(
+            "Required. 20 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
     )
     email = models.EmailField(
         verbose_name=_("email address"),
-        max_length=254,
+        max_length=250,
         blank=True
     )
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
     is_active = models.BooleanField(
-        verbose_name=_("active"),
-        default=True
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
     )
-    is_admin = models.BooleanField(
-        verbose_name=_("admin"),
-        default=False
-    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
     objects = CustomUserManager()
 
@@ -64,15 +82,9 @@ class CustomUser(AbstractBaseUser):
     EMAIL_FIELD = "email"
     REQUIRED_FIELDS = ["email"]
 
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
     def __str__(self):
         return self.username
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
-    @property
-    def is_staff(self):
-        return self.is_admin
